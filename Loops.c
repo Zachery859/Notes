bffea74d994bdcc20dae7e14e006efc0ab71c7f7
Zachery859 <za599182@ucf.edu>
2024-09-05 21:11:14 -0400

    Create Loops.c

---
+++ Loops.c
@@ -0,0 +1,62 @@
+// Kind of Loops
+//Counting Loop - When you know the amount of loops ie while,for
+//Sentinel Controlled Loop - Input a list of data, anylength, ended by a special value ie while,for
+//Endfile-controlled Loop - Input any list of data of any length from a data file (While for)
+//Input Validation Loop - Repeated interactive input of a data value until it is within desired range (do-while)
+//General Conditional Loop - Repeated processing of data until goal is met (While,for)
+
+// Increment and Decrement Operators
+// counter++ (Plus 1)
+// counter-- (Minus 1)
+
+// Preincrement/decrement (--counter) Counter value enters things -1/+1
+// Postincre/decrement (counter++) Counter value leaves loops -1/+1
+
+// Associated Keywords
+// Loops have special keywords to be used within them
+// Continue: allow an iteration to be skipped Loop 1->3
+// Break: allows the entire loop to be exited prematurely 1->done
+
+// For Loops (starting condition; Repetition Condition; Update)
+// int num_iters = 10;
+// for(int count = 0; count < num_iters; count++) {
+			// Other stuff 
+//}
+
+// While Loop
+// while(count<num_iters && num_iters > 5)
+//{
+   //insturtions go here
+//}
+
+// Do { Executes at least once
+			// do stuff
+//} while(condition);
+
+// Example Looping
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
